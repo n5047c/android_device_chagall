2225303a345216b03f39703320d07253e83e70e0
Вячеслав Роенко <roenko_vv@transset.ru>
2014-10-02 10:07:57 +0400

    remove files for tf201
    Change-Id: Id5cae1c36e4366c7c2f75bd28c62ccb8c5353398

--- wifimacwriter/Android.mk
+++
@@ -1,7 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-LOCAL_MODULE := wifimacwriter
-LOCAL_SRC_FILES := wifimacwriter.c
-LOCAL_MODULE_TAGS := optional
-LOCAL_SHARED_LIBRARIES := libcutils
-include $(BUILD_EXECUTABLE)
